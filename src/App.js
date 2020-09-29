1de1e50d51312d854427f2734ad7ae509d234d65
olamide <lamideoye@gmail.com>
2020-09-29 23:42:44 +0100

    added travis file

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed again!
+          learn react!
         </a>
       </header>
     </div>
